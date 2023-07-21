316ce3829ec6faccb8d36ae074f7a9df518e6547
Ridhi Chhajer <ridhichhajer11@gmail.com>
2023-07-21 17:41:49 +0530

    image alts

--- src/components/card/Card.jsx
+++ src/components/card/Card.jsx
@@ -6,7 +6,7 @@ function Card () {
     return (
         <div className="Card">
             <div className="Image">
-                <img src="https://images.unsplash.com/photo-1541963463532-d68292c34b19?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2788&q=80" alt="image"></img>
+                <img src="https://images.unsplash.com/photo-1541963463532-d68292c34b19?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2788&q=80" alt="book"></img>
             </div>
             <div className="Icons">
                 <div className="Wishlist">
